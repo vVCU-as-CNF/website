d3266ac5136141f018f8f6b5895dfdcdd9c3d942
Miguel Matos <miguelamatos@ua.pt>
2023-03-20 16:15:23 +0000

    a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/logo.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://vvcu-as-cnf.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/website/",
